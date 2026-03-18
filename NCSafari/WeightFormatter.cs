023a933434d41a27d16156747ec7dddb2969f41e
razer <62425425+lleaob@users.noreply.github.com>
2026-03-18 20:13:50 +0000

    feat: Format animal weight according to region.

---
+++ NCSafari/WeightFormatter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCSafari
+{
+    internal class WeightFormatter : IFormattable<Animal>
+    {
+        public WeightFormatter() { }
+
+
+        public string ToString(string? format, IFormatProvider? formatProvider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
