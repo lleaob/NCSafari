13a521a2cccd14181518c81c9d80772bda0bb9c5
Yosefgid <jossykasshun@yahoo.com>
2026-03-18 11:13:17 +0000

    Add a class Animal

---
+++ NCSafari/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCSafari
+{
+    public abstract class Animal
+    {
+       // Float to store weight
+        public float Weight { get; set; }
+        public Animal(float weight)
+        {
+            Weight = weight;
+        }
+        public abstract void MakeSound();
+    }
+}
