f4712dfe8704e8e85489788c6efd968297162651
razer <62425425+lleaob@users.noreply.github.com>
2026-03-18 20:10:55 +0000

    fix: Adjusting input to align with the Animal class.

--- NCSafari/Program.cs
+++ NCSafari/Program.cs
@@ -1,24 +1,32 @@
-﻿namespace NCSafari
+﻿using System.Globalization;
+
+namespace NCSafari
 {
     internal class Program
     {
         public static void Main()
         {
-            List<Animal> animalsToSee = new List<Animal>
-            {
-                new Tiger(70),
-                new Goose(5.2f),
-                new Bat(0.9f),
-                new Goose(4),
-                new Tiger(90),
-                new Penguin(15.8f)
-            };
+            //List<Animal> animalsToSee = new List<Animal>
+            //{
+            //    new Tiger(70),
+            //    new Goose(5.2f),
+            //    new Bat(0.9f),
+            //    new Goose(4),
+            //    new Tiger(90),
+            //    new Penguin(15.8f)
+            //};
 
-            animalsToSee.Sort(new CompareWeight(true));
-            animalsToSee.ForEach(animal => Console.WriteLine($"{animal.GetType().Name} - {animal.Weight}kg"));
+            //animalsToSee.Sort(new CompareWeight(true));
+            //animalsToSee.ForEach(animal => Console.WriteLine($"{animal.GetType().Name} - {animal.Weight}kg"));
 
             //animalsToSee.Sort(new CompareWeight(false));
             //animalsToSee.ForEach(animal => Console.WriteLine($" {animal.GetType().Name} - {animal.Weight}kg"));
+
+            Tiger pumpkin = new Tiger((float)100.4); //Casting the number into a float as Animal Weight is a float
+            CultureInfo culture = new CultureInfo("es-ES");
+
+            pumpkinWeightFormatter = new WeightFormatter(pumpkin, culture);
+
 
         }
     }
