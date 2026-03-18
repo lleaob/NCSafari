1997a6489e79b48f0fd43b70c8e4a9d3d6a85e38
Yosefgid <jossykasshun@yahoo.com>
2026-03-18 11:45:31 +0000

    Add a concrete tiger class

---
+++ NCSafari/Tiger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCSafari
+{
+    public class Tiger : PantheraBigCat, IMove
+    {
+        public Tiger(float weight) : base(weight)
+        {
+
+        }
+
+        //We have to implemnt IMove method Move() by declaring it, otherwise error
+
+        public void Move()
+        {
+            Console.WriteLine("Tiger is Moving!");
+        }
+    }
+}
