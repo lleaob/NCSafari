a03eaf88f05bab7ab5107dbfcbe33da3c7c2d8ad
Yosefgid <jossykasshun@yahoo.com>
2026-03-18 11:52:15 +0000

    Minor Changes

--- NCSafari/Tiger.cs
+++ NCSafari/Tiger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,5 +20,6 @@ namespace NCSafari
         {
             Console.WriteLine("Tiger is Moving!");
         }
+
     }
 }
